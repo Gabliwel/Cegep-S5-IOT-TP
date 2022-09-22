6717a3474d741bb81aa8bac14d9a01d5b8ebb77b
Cédric Bouchard <58229616+Waza-Ari2978@users.noreply.github.com>
2022-09-22 09:37:29 -0400

    aqiscale functionnal

--- TP1/lib/AqiScale/src/AqiScale.h
+++ TP1/lib/AqiScale/src/AqiScale.h
@@ -2,4 +2,12 @@ class AqiScale
 {
 public:
     AqiScale();
+    char* getAQI(int pm25);
+private:
+    char iqa1[4] = "Bon";
+    char iqa2[11] = "Acceptable";
+    char iqa3[8] = "Mauvais";
+    int meanIQA = 35;
+    int goodpm25Limit = 25;
+    int okpm25Limit = 50;
 };
