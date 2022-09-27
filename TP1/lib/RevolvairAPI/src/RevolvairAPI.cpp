04be11fd22d28a559cb98ed2344bfb8c13c65ed4
Gabriel Bertrand <gabriel_bertrand@hotmail.com>
2022-09-26 21:55:04 -0400

    correction finale minime

--- TP1/lib/RevolvairAPI/src/RevolvairAPI.cpp
+++ TP1/lib/RevolvairAPI/src/RevolvairAPI.cpp
@@ -5,13 +5,32 @@ RevolvairAPI::RevolvairAPI()
 
 }
 
+void RevolvairAPI::setup()
+{
+    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+    Serial.print(F("Waiting for NTP time sync: "));
+    time_t nowSecs = time(nullptr);
+    while (nowSecs < 8 * 3600 * 2) {
+        delay(500);
+        Serial.print(F("."));
+        yield();
+        nowSecs = time(nullptr);
+    }
+
+    Serial.println();
+    struct tm timeinfo;
+    gmtime_r(&nowSecs, &timeinfo);
+    Serial.print(F("Current time: "));
+    Serial.print(asctime(&timeinfo));
+}
+
 void RevolvairAPI::postData(uint32_t chipId, String macId, int * pmsValues, float temp, float humidity)
 {
     String jsonPm25Package = "";
     StaticJsonDocument<200> doc;
 
-    //doc["esp8266id"] = chipId;
-    doc["esp8266id"] = "12224532";
+    doc["esp8266id"] = chipId;
     doc["software_version"] = "Revo-2022-777";
     JsonArray array2 = doc.createNestedArray("sensordatavalues");
     JsonObject nested = array2.createNestedObject();
